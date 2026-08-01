baa0b4e219bc5c257a4fdfb0ba977af00d22d68e
tusharsorte3-hub <tusharsorte3@gmail.com>
2026-08-01 13:09:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function updateTime(){
+// Clock
+function updateTime() {
 
-let now=new Date();
+let now = new Date();
 
-document.getElementById("time").innerHTML=
+document.getElementById("time").innerHTML =
 now.toLocaleString();
 
 }
@@ -11,11 +12,114 @@ setInterval(updateTime,1000);
 
 updateTime();
 
+// Load Medicines
+window.onload = function(){
+
+loadMedicine();
+
+};
+
+// Add Medicine
+function addMedicine(){
+
+let name=document.getElementById("medicineNameInput").value;
+
+let time=document.getElementById("medicineTimeInput").value;
+
+if(name=="" || time==""){
+
+alert("Enter Medicine Name and Time");
+
+return;
+
+}
+
+let medicines=
+JSON.parse(localStorage.getItem("medicine")) || [];
+
+medicines.push({
+
+name:name,
+
+time:time
+
+});
+
+localStorage.setItem("medicine",
+
+JSON.stringify(medicines));
+
+document.getElementById("medicineNameInput").value="";
+
+document.getElementById("medicineTimeInput").value="";
+
+loadMedicine();
+
+}
+
+// Display Medicines
+
+function loadMedicine(){
+
+let medicines=
+JSON.parse(localStorage.getItem("medicine")) || [];
+
+let table=document.querySelector("#medicineTable tbody");
+
+table.innerHTML="";
+
+medicines.forEach((item,index)=>{
+
+table.innerHTML+=`
+
+<tr>
+
+<td>${item.name}</td>
+
+<td>${item.time}</td>
+
+<td>
+
+<button class="deleteBtn"
+
+onclick="deleteMedicine(${index})">
+
+Delete
+
+</button>
+
+</td>
+
+</tr>
+
+`;
+
+});
+
+}
+
+// Delete Medicine
+
+function deleteMedicine(index){
+
+let medicines=
+JSON.parse(localStorage.getItem("medicine"));
+
+medicines.splice(index,1);
+
+localStorage.setItem("medicine",
+
+JSON.stringify(medicines));
+
+loadMedicine();
+
+}
+
+// Existing Buttons
+
 function openDoor(){
 
 document.getElementById("doorStatus").innerHTML="Opened";
-
-alert("Medicine Box Opened");
 
 }
 
@@ -23,14 +127,10 @@ function closeDoor(){
 
 document.getElementById("doorStatus").innerHTML="Closed";
 
-alert("Medicine Box Closed");
-
 }
 
 function stopAlarm(){
 
 document.getElementById("alarmStatus").innerHTML="OFF";
-
-alert("Alarm Stopped");
 
 }
