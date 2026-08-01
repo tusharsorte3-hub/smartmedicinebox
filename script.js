0d224683766b2f8b9f6fa34b40985e4bff5d9045
tusharsorte3-hub <tusharsorte3@gmail.com>
2026-08-01 13:03:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,119 @@
-function updateClock(){
+*{
 
-let now=new Date();
-
-let time=now.toLocaleTimeString();
-
-document.getElementById("clock").innerHTML=time;
+margin:0;
+padding:0;
+box-sizing:border-box;
+font-family:Arial,sans-serif;
 
 }
 
-setInterval(updateClock,1000);
+body{
 
-updateClock();
+background:#f2f7ff;
 
-document.getElementById("espStatus").innerHTML="Waiting...";
+}
 
-document.getElementById("doorStatus").innerHTML="Closed";
+header{
 
-document.getElementById("rfidStatus").innerHTML="No Card";
+background:#1565c0;
+color:white;
 
-document.getElementById("alarmStatus").innerHTML="OFF";
+padding:20px;
+
+text-align:center;
+
+}
+
+#time{
+
+margin-top:10px;
+
+font-size:20px;
+
+}
+
+.container{
+
+width:90%;
+
+max-width:1000px;
+
+margin:20px auto;
+
+display:grid;
+
+grid-template-columns:repeat(auto-fit,minmax(300px,1fr));
+
+gap:20px;
+
+}
+
+.card{
+
+background:white;
+
+padding:20px;
+
+border-radius:10px;
+
+box-shadow:0 5px 10px rgba(0,0,0,.2);
+
+}
+
+.card h2{
+
+margin-bottom:15px;
+
+color:#1565c0;
+
+}
+
+.card p{
+
+margin:12px 0;
+
+font-size:18px;
+
+}
+
+button{
+
+width:100%;
+
+padding:12px;
+
+margin-top:10px;
+
+font-size:17px;
+
+background:#1565c0;
+
+color:white;
+
+border:none;
+
+border-radius:6px;
+
+cursor:pointer;
+
+}
+
+button:hover{
+
+background:#0d47a1;
+
+}
+
+footer{
+
+text-align:center;
+
+padding:20px;
+
+background:#1565c0;
+
+color:white;
+
+margin-top:20px;
+
+}
